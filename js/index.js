3a12032d36d44a0d3ac77711d3fd4ab513bbc152
Yash Malviya <malviya.yash18@gmail.com>
2021-03-21 22:36:15 +0530

    Video added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
-$("#aboutUsButton").click(function () {
-    location.href = "aboutUs.html"
+document.querySelectorAll("#second-nav-middle div.item-cat:nth-child(5)")[0].addEventListener('click', function (event) {
+    window.location.href = "./aboutUs.html";
+});
+
+document.querySelectorAll("#second-nav-middle div.item-cat:nth-child(4)")[0].addEventListener('click', function (event) {
+    window.location.href = "./partners.html";
+});
+
+document.querySelectorAll("#second-nav-middle div.item-cat:nth-child(3)")[0].addEventListener('click', function (event) {
+    window.location.href = "./blog.html";
+});
+
+document.querySelectorAll("#second-nav-middle div.item-cat:nth-child(2)")[0].addEventListener('click', function (event) {
+    window.location.href = "./services.html";
+});
+
+document.querySelectorAll("#second-nav-middle div.item-cat:nth-child(1)")[0].addEventListener('click', function (event) {
+    window.location.href = "/";
 });
