fc48fc02d05d101df80f0aff17d1b3a18443a6e3
Yash Malviya <malviya.yash18@gmail.com>
2021-03-24 23:00:25 +0530

    Test code for https://onlineprograms.sacredheart.edu/

--- js/index.js
+++ js/index.js
@@ -17,3 +17,74 @@ document.querySelectorAll("#second-nav-middle div.item-cat:nth-child(2)")[0].add
 document.querySelectorAll("#second-nav-middle div.item-cat:nth-child(1)")[0].addEventListener('click', function (event) {
     window.location.href = "/";
 });
+
+
+// Test Code for https://onlineprograms.sacredheart.edu/
+
+window.addEventListener('beforeunload', recordExitTime);
+
+
+var visitedPage = [];
+var pageTime = [];
+var visitTime;
+var exitTime;
+
+$('document').ready(function () {
+
+    visitTime = new Date();
+    getCountry();
+
+    //  Check for data in session storage. (same session)
+
+    if (sessionStorage.getItem('visitedPage') !== null && sessionStorage.getItem('pageTime') !== null) {
+        visitedPage = JSON.parse(sessionStorage.getItem('visitedPage'));
+        pageTime = JSON.parse(sessionStorage.getItem('pageTime'))
+    } else { // (new session)
+
+    }
+});
+
+$('window').unload(function () {
+    recordExitTime();
+});
+
+
+
+// Functions 
+
+function addCurrentPath() {
+    visitedPage.push(window.location.pathname);
+    sessionStorage.setItem('visitedPage', JSON.stringify(visitedPage));
+}
+
+function calculateTimeSpent() {
+    let spentTime = Math.round((exitTime - visitTime) / 1000);
+    return spentTime;
+}
+
+function recordExitTime() {
+    exitTime = new Date();
+    pageTime.push(calculateTimeSpent());
+    addCurrentPath();
+    sessionStorage.setItem('pageTime', JSON.stringify(pageTime));
+}
+
+function getPageLang() {
+    var lang = document.documentElement.lang;
+    console.log(lang);
+    return lang;
+}
+
+function getCountry() {
+    $.ajax('http://ip-api.com/json')
+        .then(
+            function success(response) {
+                console.log(response.country);
+            },
+
+            function fail(data, status) {
+                console.log("Failed ;(");
+            }
+        );
+}
+
